f919241ad4b5443732278ff14d73ede142612027
GonzaloMonteodorisio <56704895+GonzaloMonteodorisio@users.noreply.github.com>
2022-06-26 12:42:12 -0300

    comment bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import Animate from 'animate.css-react';
- import 'animate.css/animate.css';
+//  import 'animate.css/animate.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import NavBar from './components/NavBar';
